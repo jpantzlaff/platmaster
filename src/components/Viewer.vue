fc4ec49ee034aff02bc1a8f1262b8aff3f6e3029
pantzer20 <pantzlaff@wisc.edu>
2020-04-25 16:45:02 -0500

    Start viewer; Remove unneeded dependencies

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="viewer">
-    
+    <div class="ol"></div>
+    <div
+      class="switcher"
+      v-if="state.pages.length > 1"
+    >
+      <UiIconButton
+        class="advance"
+        icon="arrow_back_ios"
+        size="mini"
+        tooltip="View previous page"
+        :disabled="state.activePage.id <= 1"
+        v-on:click="backward"
+      />
+      <p class="page-number">{{state.activePage.id}}</p>
+      <UiIconButton
+        class="advance"
+        icon="arrow_forward_ios"
+        size="mini"
+        tooltip="View next page"
+        :disabled="state.activePage.id >= state.pages.length"
+        v-on:click="forward"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+  .switcher {
+    bottom: 2rem;
+    display: flex;
+    left: 2rem;
+    position: absolute;
+  }
 </style>
 
 <script>
 
-// import {
-//   UiButton,
-//   UiDatepicker,
-//   UiFileupload,
-//   UiSelect,
-//   UiTextbox
-// } from 'keen-ui';
+import {
+  UiIconButton
+} from 'keen-ui';
 import 'keen-ui/dist/keen-ui.css';
 
 import {distanceUnits, magDeclinationYears} from '../env.js';
@@ -32,15 +56,20 @@ export default {
     return {state};
   },
   methods: {
+    backward() {
+
+    },
+    forward() {
+
+    }
   },
   watch: {
+    'state.activePage': function(value) {
+
+    }
   },
   components: {
-    // UiButton,
-    // UiDatepicker,
-    // UiFileupload,
-    // UiSelect,
-    // UiTextbox
+    UiIconButton
   }
 };
 
